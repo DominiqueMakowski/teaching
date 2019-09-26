8e0a0ab3ff70ae3b9950e44abd6554725ad98156
zen-juen <54053748+zen-juen@users.noreply.github.com>
2019-09-26 17:53:37 +0800

    Create install_packages.R

---
+++ R/2019_09_CBL_workshop/materials/install_packages.R
@@ -0,0 +1,12 @@
+#Install packages
+install.packages(c("devtools", "tidyverse", "knitr", "lme4", "plotly", "BayesFactor", "rstanarm"))
+devtools::install_github("easystats/easystats")
+
+#Load packages
+library(tidyverse)
+library(easystats)
+library(knitr)
+library(lme4)
+library(plotly)
+library(BayesFactor)
+library(rstanarm)
