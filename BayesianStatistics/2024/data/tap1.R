ed94e3bd488a6a0228529d602ee2ae2f2ff65013
Dominique Makowski <dom.mak19@gmail.com>
2024-01-10 21:50:41 +0000

    Update tap1.R

--- BayesianStatistics/2024/data/tap1.R
+++ BayesianStatistics/2024/data/tap1.R
@@ -1,5 +1,5 @@
 set.seed(1)
 df <- data.frame(rt=rgamma(5000, 2.5, 1))
-df$conf <- c(rbeta(2500, 0.5, 2) / 2, (rbeta(2500, 2, 0.5) / 2) + 0.5)
+df$confidence <- c(rbeta(2500, 0.5, 2) / 2, (rbeta(2500, 2, 0.5) / 2) + 0.5)
 
 write.csv(df, "tap1a.csv", row.names=FALSE)
