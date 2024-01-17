c6f38dce9280129dd1e104c2f7b6093d6b3b97d5
Dominique Makowski <dom.mak19@gmail.com>
2024-01-17 13:35:30 +0000

    add resit data

--- BayesianStatistics/2024/data/tap1.R
+++ BayesianStatistics/2024/data/tap1.R
@@ -3,3 +3,9 @@ df <- data.frame(rt=rgamma(5000, 2.5, 1))
 df$confidence <- c(rbeta(2500, 0.5, 2) / 2, (rbeta(2500, 2, 0.5) / 2) + 0.5)
 
 write.csv(df, "tap1a.csv", row.names=FALSE)
+
+set.seed(2)
+df <- data.frame(reading_time=rgamma(5000, 2.5, 1))
+df$certainty <- c(rbeta(2500, 0.5, 2) / 2, (rbeta(2500, 2, 0.5) / 2) + 0.5)
+
+write.csv(df, "tap1a_resit.csv", row.names=FALSE)
